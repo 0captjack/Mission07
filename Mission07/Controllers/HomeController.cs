2b102f8507da13ea9104be1c448c66e8c4d5fe71
0captjack <85979485+0captjack@users.noreply.github.com>
2022-02-17 18:31:00 -0700

    07 pulling data

--- Mission07/Controllers/HomeController.cs
+++ Mission07/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Mission07.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +8,22 @@ using System.Threading.Tasks;
 namespace Mission07.Controllers
 {
     public class HomeController : Controller
-    {
+    {           
+        private BookstoreContext context { get; set; }
+
+        public HomeController (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        
+        /*public IActionResult Index() => View();*/
         public IActionResult Index()
         {
-            return View();
+            var variable1 = context.Books.ToList();
+            
+            return View(variable1);
         }
+
+        
     }
 }
--- Mission07/Models/Book.cs
+++ Mission07/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -9,14 +10,17 @@ namespace Mission07.Models
 {
     public partial class Book
     {
-        public long BookId { get; set; }
+        [Key]
+        [Required]
+        public int BookId { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Author { get; set; }
         public string Publisher { get; set; }
         public string Isbn { get; set; }
         public string Classification { get; set; }
         public string Category { get; set; }
-        public long PageCount { get; set; }
+        public int PageCount { get; set; }
         public double Price { get; set; }
     }
 }
