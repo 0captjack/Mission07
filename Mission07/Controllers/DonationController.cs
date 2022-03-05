3dd835e424e094a30473ae5e50e996b51f0cd8af
0captjack <85979485+0captjack@users.noreply.github.com>
2022-03-04 21:09:11 -0700

    checkout view

---
+++ Mission07/Controllers/DonationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission07.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission07.Controllers
+{
+    public class DonationController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View(new Donation());
+        }
+    }
+}
